0865b7209109aa9a16292a788603ffa46deb1e83
Mathew Dela Cruz <105842384+MatDelaCruz@users.noreply.github.com>
2022-05-20 11:20:59 +0800

    first commit

---
+++ Angry.py
@@ -0,0 +1,5 @@
+angry=input("Are you angry?")
+if hungry=="yes";
+    print("go shower")
+else:
+    print("let's talk")
---
+++ Hungry.py
@@ -0,0 +1,5 @@
+hungry=input("Are you hungry?")
+if hungry=="yes";
+    print("eat donut")
+else:
+    print("Get some exercise")
